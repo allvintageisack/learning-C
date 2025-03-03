b7e891355e4236f285cf0166bde8d8c4b281fdb3
allvintageisak <mwangasuisack@gmail.com>
2025-03-03 09:17:16 +0300

    started on if statements

---
+++ ifstatment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int Marks;
+    printf("Enter mark \n");
+    scanf("%d", &Marks);
+    if(Marks>=40)
+    {
+        printf("You have passed");
+
+    }
+    return 0;
+}
+
+// The if statement is used to execute a block of code only if a condition is true.
