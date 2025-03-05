f745de7d39c15e4e1831631fcb0b356d16a59d4e
allvintageisak <mwangasuisack@gmail.com>
2025-03-05 08:10:01 +0300

    formatspesifiers

--- minor Work/display.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main ()
-{
-    printf("%d\n", 128);        
-    printf("%f\n", 128.0);    
-    printf("%f\n", 167.5);    
-
-    return 0;
-}
---
+++ minor Work/formatspecifiers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//format specifiers in c
+// &d is used inside the printf() function to print integer values,
+// %f is used to print float values,
+// %c is used to print character values,
+// %s is used to print string values.
+int main ()
+{
+    int num1;
+    num1 = 17;
+    printf("%d", num1);    
+// so all together 
+int secondnum =6;
+float secondnum2 = 6.55;
+char myletter = 'A'; // note in char we use single quotes and not double quotes
+
+printf("%d", secondnum);
+printf("%f", secondnum2);
+    return 0;
+}
