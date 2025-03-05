2c752fb8f889a9ad2e08c69ce9a6d33d22128efd
allvintageisak <mwangasuisack@gmail.com>
2025-03-05 06:07:39 +0300

    answering online question

--- minor Work/escape sequence.c
+++ minor Work/escape sequence.c
@@ -4,6 +4,14 @@ int main() {
     printf("i'm confused , what does ivy want\t");
     printf("i kinda like her");
 // \t creates a horizontal tab
+
+
+    int number1 = 33;
+    printf("%d ", number1);
+    printf("%d", number1);
+
     return 0;
+
 }
 /*this is a multi-line comments*/
+
